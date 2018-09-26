29d100fc356cb997352d58cd36509eaef68185a5
tchan2 <43620531+tchan2@users.noreply.github.com>
2018-09-26 19:41:22 -0400

    Create rectangle.cpp

---
+++ 2017-02-10/rectangle.cpp
@@ -0,0 +1,19 @@
+rectangle :: get_perimeter()
+{
+    return ((2*width) + (2*length));
+}
+
+rectangle :: get area()
+{
+    return width*length;
+}
+
+rectangle :: set_width (double w)
+{
+    width = w;
+}
+
+rectangle :: set_length (double l)
+{
+    length = l;
+}
