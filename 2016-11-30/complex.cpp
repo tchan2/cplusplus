478403f8478c831342942c554a31be614107c68f
tchan2 <43620531+tchan2@users.noreply.github.com>
2018-09-26 21:08:54 -0400

    Create complex.cpp

---
+++ 2016-11-30/complex.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <cmath>
+#include "complex.h"
+
+using namespace std;
+
+complex :: complex (double int_real, double int_imag)
+{
+    real = int_real;
+    imag = int_imag;
+}
+
+complex complex :: operator+ (const complex x)
+{
+    complex temp;
+    temp.real = real + x.real;
+    temp.imag = imag + x.imag;
+    return (temp);
+}
+
+complex complex :: operator- (const complex x)
+{
+    complex temp;
+    temp.real = real - x.real;
+    temp.imag = imag - x.imag;
+    return (temp);
+}
+
+complex complex :: operator* (const complex x)
+{
+    complex temp;
+    temp.real = (real*x.real) - (imag*x.imag);
+    temp.imag = (imag*x.imag) + (real*x.real);
+    return (temp);
+}
+
+complex complex :: operator/ (const complex x)
+{
+    complex temp;
+    temp.real = (((real)*(x.real)) + ((imag)*(x.imag))) / pow(x.real, 2) + pow(x.imag, 2);
+    temp.imag = (((x.real)*(imag)) - ((real)*(x.imag))) / (pow(x.real, 2) + pow(x.imag, 2));
+}
+
+void complex :: InputC (istream &in)
+{
+    char i, paren, comma;
+    in >> paren >> real >> comma >> imag >> i >> paren;
+}
+
+void complex :: OutputC (ostream &out)
+{
+    out << '(' << real << ';' << imag << "i)" << endl;
+}
+
+istream &operator >> (istream &in, complex x)
+{
+    x.InputC(in);
+    return (in);
+}
+
+ostream &operator >> (ostream &out, complex x)
+{
+    x.OutputC(out);
+    return (out);
+}
