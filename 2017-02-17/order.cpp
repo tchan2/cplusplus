b432befcffe56b4a697c18f825b89aaededd919c
tchan2 <43620531+tchan2@users.noreply.github.com>
2018-09-26 20:42:38 -0400

    Create order.cpp

---
+++ 2017-02-17/order.cpp
@@ -0,0 +1,50 @@
+// Determine the order of three user-inputted numbers.
+
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+void determine_order (double&a, double&b, double&c);
+double a, b, c;
+
+using namespace std;
+
+int main()
+{
+    cout << fixed << showpoint << setprecision(1);
+    cout << "Enter three numbers: ";
+    cin >> a >> b >> c;
+    
+    determine_order (a, b, c);
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+    cout << "c = " << c << endl;
+}
+
+void determine_order (double&a, double&b, double&c)
+{
+    if (a > b)
+    {
+        swap (a, b);
+        if (b > c)
+            swap (b, c);
+        if (a > b)
+            swap (a, b);
+    }
+    
+    else
+    {
+        if (b > c)
+            swap (b, c);
+        if (a > b)
+            swap (a, b);
+    }
+}
+
+void swap (double&a, double&b, double&c)
+{
+    double temp;
+    temp = a;
+    a = b;
+    temp = b;
+}
